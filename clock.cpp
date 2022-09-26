9eac71993251a30493465649cf7384bfbfcaca01
Wombilisk <m.krumpeck@aon.at>
2022-09-26 22:14:40 +0200

    added while-loop

--- clock.cpp
+++ clock.cpp
@@ -6,25 +6,28 @@ void signalHandler(int sigNum) {	//change whose turn it is (SIGUSR1)
 	whoseTurn = !whoseTurn;
 }
 
-int chessClock(void) {
+int countdown(float time) {
+	return time - TIME_STEP;
+}
 
+int chessClock(void) {
+	double blackTime = 0;
+	double whiteTime = 0;
+	
 	//TODO: get values for black and white times
 	
 	signal(SIGUSR1, signalHandler);
 
-	while(true) {
+	while(blackTime > 0 && whiteTime > 0) {
 		switch(whoseTurn) {
-			case 0:
+			case 0:	//white's turn
+				countdown(whiteTime);
 				break;
-			case 1:
+			case 1: //black's turn
+				countdown(blackTime);
 				break;
 		}
 	}
 
 	return EXIT_SUCCESS;
 }
-
-int countdown(float time) {
-	return time - TIME_STEP;
-}
-
